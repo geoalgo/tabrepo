36f2a1427a4e452384b464f98955a0a8c41be86f
Lennart Purucker <purucker@amazon.com>
2023-08-23 09:33:22 -0700

    update unclean code for experiments

--- scripts/leakage_benchmark/src/custom_metadata_funcs.py
+++ scripts/leakage_benchmark/src/custom_metadata_funcs.py
@@ -14,6 +14,12 @@ def _sub_sample(l2_train_data, l2_test_data, sample=20000, sample_test=10000):
     return l2_train_data, l2_test_data
 
 
+def _get_ag_cv(layer):
+    from autogluon.core.utils.utils import CVSplitter
+
+    cv = CVSplitter(n_splits=8, n_repeats=1, stratified=True, random_state=layer)
+    return cv
+
 def _compute_nearest_neighbor_distance(X_train, y_train, X_test):
     # Compute nearest neighbor distance
     from autogluon.core.utils.utils import CVSplitter
@@ -271,6 +277,382 @@ def _all_wrong_count(X, y, oof_col_names, threshold=0.5):
     return sum(s_tmp != y[no_diversity_rows]) / len(X)
 
 
+def to_label(proba):
+    arr = proba.values
+
+    if len(arr.shape) == 1:
+        arr = np.vstack([1 - arr, arr]).T
+    return np.argmax(arr, axis=1)
+
+
+def _linkage_matrix(model):
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, np.full_like(counts, np.nan), counts]
+    ).astype(float)
+
+    return linkage_matrix
+
+
+def _cluster_plot(model, **kwargs):
+    from scipy.cluster.hierarchy import dendrogram
+    from matplotlib import pyplot as plt
+
+    # from https://scikit-learn.org/stable/auto_examples/cluster/plot_agglomerative_dendrogram.html#sphx-glr-auto-examples-cluster-plot-agglomerative-dendrogram-py
+    # Create linkage matrix and then plot the dendrogram
+
+    # Plot the corresponding dendrogram
+    dendrogram(_linkage_matrix(model), **kwargs)
+    plt.show()
+
+
+def _preprocessor_for_cluster():
+    from sklearn.compose import ColumnTransformer
+    from sklearn.compose import make_column_selector
+    from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+    from sklearn.impute import SimpleImputer
+    from sklearn.pipeline import Pipeline
+
+    preprocessor = Pipeline(
+        [
+            ('fix', ColumnTransformer(transformers=[
+                ("num", SimpleImputer(strategy="constant", fill_value=-1),
+                 make_column_selector(dtype_exclude="object"),),
+                ("cat",
+                 Pipeline(steps=[("encoder", OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1),),
+                                 ("imputer", SimpleImputer(strategy="constant", fill_value=-1)),
+                                 ]), make_column_selector(dtype_include="object"),), ], sparse_threshold=0, )),
+            ('scale', StandardScaler())
+        ]
+    )
+
+    return preprocessor
+
+
+def wrong_likelihood(data, label_col, oof_col_names, groups):
+    from scipy.stats import binom
+    from sklearn.metrics import accuracy_score
+
+    global_acc_map = {oof_col: accuracy_score(data[label_col], to_label(data[oof_col])) for oof_col in oof_col_names}
+
+    likelihood_correct_per_oof = []
+    for group_i in groups:
+        mask = groups == group_i
+        subset = data[mask]
+
+        likelihood_correct_for_subset_per_oof = []
+        for oof_col in oof_col_names:
+            alpha = sum(to_label(subset[oof_col]) == subset[label_col])
+            likelihood_correct_for_subset_per_oof.append(binom.pmf(alpha, len(subset), global_acc_map[oof_col]))
+
+        likelihood_correct_per_oof.append(likelihood_correct_for_subset_per_oof)
+
+    return np.mean(np.mean(np.array(likelihood_correct_per_oof), axis=0))
+
+
+def _value_expectation(new_oof, oof, y, i, lm, total_n_samples, _lambda, tol_range):
+    left_i, right_i, distance, n_samples = lm[i]
+
+    # FIXME, change lm array type instead of cast here
+    left_i = int(left_i)
+    right_i = int(right_i)
+
+    if left_i < total_n_samples:
+        left_i_list, left_sum = [left_i], oof[left_i]
+    else:
+        left_i_list, left_sum = _value_expectation(new_oof, oof, y, left_i - total_n_samples, lm, total_n_samples, _lambda, tol_range)
+
+    if right_i < total_n_samples:
+        right_i_list, right_sum = [right_i], oof[right_i]
+    else:
+        right_i_list, right_sum = _value_expectation(new_oof, oof, y, right_i - total_n_samples, lm, total_n_samples,
+                                                     _lambda, tol_range)
+
+    all_i = left_i_list + right_i_list
+    sum_vals = left_sum + right_sum
+
+    left_i_exp = left_sum / len(left_i_list)
+    right_i_exp = right_sum / len(right_i_list)
+    level_exp = sum_vals / n_samples
+
+    # l_shrinkage, r_shrinkage = (1 + _lambda / len(left_i_list)), (1 + _lambda / len(right_i_list))
+    shrinkage = (1 + _lambda/n_samples)
+    # tol_range = 0.2
+    new_oof[left_i_list] += (left_i_exp - level_exp) / shrinkage
+    new_oof[right_i_list] += (right_i_exp - level_exp)/ shrinkage
+
+    return all_i, sum_vals
+
+
+def hierarchical_oof_shrinkage(X, oof_col_names):
+    from scipy.cluster.hierarchy import linkage
+
+    lm = linkage(_preprocessor_for_cluster().fit_transform(X.drop(columns=oof_col_names)),
+                 metric='euclidean', optimal_ordering=False, method='ward')
+
+    _lambda = 100
+    for oof_col in oof_col_names:
+        oof = X[oof_col].values
+        new_oof = np.full_like(oof, np.mean(oof))
+        _value_expectation(new_oof, oof, -1, lm, len(oof), _lambda)
+        X[oof_col] = new_oof
+
+
+def _get_cheat_likelihood(data: pd.DataFrame, label_col: str, oof_col_names: list[str], eval_metric) -> float:
+
+    from sklearn.cluster import AgglomerativeClustering
+    from scipy.cluster.hierarchy import linkage
+
+    cluster_model = AgglomerativeClustering(n_clusters=50, compute_distances=False)
+    cluster_model = cluster_model.fit(_preprocessor_for_cluster().fit_transform(data.drop(columns=[label_col])))
+    lm = _linkage_matrix(cluster_model)
+    # lm = linkage(_preprocessor_for_cluster().fit_transform(data.drop(columns=oof_col_names + [label_col])),
+    #              metric='euclidean', optimal_ordering=False, method='ward')
+
+    _lambda = 100
+
+    for oof_col in oof_col_names:
+        oof = data[oof_col].values
+        new_oof = np.full_like(oof, np.mean(oof))
+        _value_expectation(new_oof, oof, data[label_col],  -1, lm, len(oof), _lambda, 0)
+        data[oof_col] = new_oof
+        # print(np.mean(abs(oof - new_oof)))
+
+        # print(f'\n ### {oof_col}')
+        # print(eval_metric(data[label_col], data[oof_col]))
+        #
+        # best_oof = oof.copy()
+        # best_score = eval_metric(data[label_col], oof)
+        # for _lambda in [1, 5, 25, 50, 100, 500]:
+        #     new_oof = np.full_like(oof, np.mean(oof))
+        #     _value_expectation(new_oof, oof, -1, lm, len(oof), _lambda)
+        #
+        #     if eval_metric(data[label_col], new_oof) > best_score:
+        #         best_oof = new_oof.copy()
+        #
+        # print(eval_metric(data[label_col], oof), eval_metric(data[label_col], best_oof))
+        # data[oof_col] = best_oof
+        # # print(eval_metric(data[label_col], data[oof_col]))
+
+    return 0
+    exit(-1)
+    print()
+
+    # groups = cluster_model.fit(_preprocessor_for_cluster().fit_transform(data.drop(columns=[label_col])))
+
+    print(wrong_likelihood(data, label_col, oof_col_names,
+                           groups
+                           # data.groupby(list(data.columns)).ngroup()
+                           ))
+
+    # from sklearn.metrics import accuracy_score
+
+    #
+    # global_acc_map = {oof_col: accuracy_score(data[label_col], to_label(data[oof_col])) for oof_col in oof_col_names}
+    # [np.mean(data[oof_col]) for oof_col in oof_col_names]
+    #
+    # # Get acc over clusters
+
+    #
+    # _cluster_plot(cluster_model, truncate_mode="level", p=3)
+    # _cluster_plot(cluster_model, truncate_mode="level", p=30)
+    # def subset_func(subset_df):
+    #     print()
+    #
+    # data.groupby(list(data.columns)).apply(subset_func)
+    return 0
+
+
+def _weight_vector_merge_exp():
+    # -- Weight Vector merger exp
+    from collections import Counter
+    def _calculate_final_weights(indices_, num_input_models_):
+        ensemble_members = Counter(indices_).most_common()
+        weights = np.zeros(
+            (num_input_models_,),
+            dtype=np.float64,
+        )
+        for ensemble_member in ensemble_members:
+            weight = float(ensemble_member[1]) / len(indices_)
+            weights[ensemble_member[0]] = weight
+
+        if np.sum(weights) < 1:
+            weights = weights / np.sum(weights)
+
+        return weights
+
+    # Idea merge OOF predictions into new vectors that obscures the leak without losing information
+    new_oofs = []
+    # rng = np.random.RandomState(42)
+    for idx, oof_col in enumerate(oof_col_names):
+
+        p = np.ones(len(oof_col_names))
+        p[idx] += 1
+        p = p/p.sum()
+
+        # # per row
+        # x = _calculate_final_weights(rng.choice(len(oof_col_names), len(oof_col_names), replace=True, p=p),
+        #                          len(oof_col_names))
+        # rng.choice(len(oof_col_names), len(oof_col_names), replace=True, p=p)
+        X_train[oof_col] = np.average(X_train[oof_col_names], axis=1, weights=p)
+        X_test[oof_col] = np.average(X_test[oof_col_names], axis=1, weights=p)
+
+def oof_melt(train_oof_df, test_oof_df, fold_indicator, value_name, ):
+    oof_df = train_oof_df.copy()
+    oof_df_p2 = test_oof_df.copy()
+
+    oof_df.loc[:, 'fold'] = 'Fold ' + pd.Series(fold_indicator.astype(int).astype(str))
+    oof_df = oof_df.melt(id_vars=['fold'], var_name='model', value_name=value_name)
+
+    oof_df_p2.loc[:, 'fold'] = 'Test'
+    oof_df_p2 = oof_df_p2.melt(id_vars=['fold'], var_name='model', value_name=value_name)
+    oof_df = pd.concat([oof_df, oof_df_p2])
+    return oof_df
+
+def _dist_overview_plot(train_oof_df, test_oof_df, fold_indicator, models_to_plot):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
+
+    value_name = 'Observed Prediction Values'
+    order = [f'Fold {int(i)}' for i in range(int(max(fold_indicator)) +1)] + ['Test']
+
+    oof_df = oof_melt(train_oof_df, test_oof_df, fold_indicator, value_name)
+
+    plot_df = oof_df[oof_df['model'].isin(models_to_plot)]
+    # Initialize the FacetGrid object
+    pal = sns.cubehelix_palette(9, rot=-.25, light=.7)
+    pal[-1] = [1, 0, 0]
+    g = sns.FacetGrid(plot_df, row="fold", hue="fold", aspect=15, height=1, palette=pal,
+                      row_order=order, hue_order=order)
+
+    # Draw the densities in a few steps
+    g.map(sns.kdeplot, value_name,
+          bw_adjust=.5, clip_on=False,
+          fill=True, alpha=1, linewidth=1.5)
+    g.map(sns.kdeplot, value_name, clip_on=False, color="w", lw=2, bw_adjust=.5)
+
+    # passing color=None to refline() uses the hue mapping
+    g.refline(y=0, linewidth=2, linestyle="-", color=None, clip_on=False)
+
+    # Define and use a simple function to label the plot in axes coordinates
+    def label(x, color, label):
+        ax = plt.gca()
+        ax.text(0, .2, label, fontweight="bold", color=color,
+                ha="left", va="center", transform=ax.transAxes)
+
+    g.map(label, value_name)
+    # Set the subplots to overlap
+    g.figure.subplots_adjust(hspace=-.25)
+
+    # Remove axes details that don't play well with overlap
+    g.set_titles("")
+    g.set(yticks=[], ylabel="")
+    g.despine(bottom=True, left=True)
+    g.fig.suptitle(f'Distribution of predictions')
+    return g
+
+def _stack_hist_plot(train_oof_df, test_oof_df, hue_indicator, models_to_plot):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    pal = sns.cubehelix_palette(9, rot=-.25, light=.7)
+    pal[-1] = [1, 0, 0]
+    value_name = 'Observed Prediction Values'
+    oof_df = oof_melt(train_oof_df, test_oof_df, hue_indicator, value_name)
+    plot_df = oof_df[oof_df['model'].isin(models_to_plot)]
+
+    sns.set_theme(style="ticks")
+
+    f, ax = plt.subplots(figsize=(15, 8))
+    ax.set_title(f'Overlap of Predictions')
+
+    g = sns.histplot(
+        data=plot_df,
+        x=value_name, hue="fold",
+        multiple="stack",
+        palette=pal,
+        edgecolor=".3",
+        linewidth=.5,
+        ax= ax
+        # log_scale=True,
+    )
+    return g
+
+
+def _stack_hist_plot_facet(train_oof_df, y_train, test_oof_df, y_test, fold_indicator, models_to_plot):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
+
+    test_oof_df = test_oof_df.copy()
+    train_oof_df.loc[:, 'y'] = y_train
+    test_oof_df = test_oof_df.copy()
+    test_oof_df.loc[:, 'y'] = y_test
+
+    value_name = 'Observed Prediction Values'
+    order = [f'Fold {int(i)}' for i in range(int(max(fold_indicator)) +1)] + ['Test']
+
+    oof_df = train_oof_df.copy()
+    oof_df_p2 = test_oof_df.copy()
+
+    oof_df.loc[:, 'fold'] = 'Fold ' + pd.Series(fold_indicator.astype(int).astype(str))
+    oof_df = oof_df.melt(id_vars=['fold', 'y'], var_name='model', value_name=value_name)
+
+    oof_df_p2.loc[:, 'fold'] = 'Test'
+    oof_df_p2 = oof_df_p2.melt(id_vars=['fold', 'y'], var_name='model', value_name=value_name)
+    oof_df = pd.concat([oof_df, oof_df_p2])
+
+    plot_df = oof_df[oof_df['model'].isin(models_to_plot)]
+
+    g = sns.displot(
+        plot_df, x=value_name, row="y", col="fold", height=3, aspect=0.9,
+        facet_kws=dict(margin_titles=True), col_order=order, kde=True,
+    )
+
+    return g
+
+
+
+
+def _plot_prediction_distributions(train_oof_df, y_train, test_oof_df, y_test, fold_indicator):
+    import matplotlib.pyplot as plt
+    models = ['L1/OOF/NeuralNetFastAI_c1_BAG_L1', 'L1/OOF/CatBoost_c1_BAG_L1', 'L1/OOF/RandomForest_c1_BAG_L1'] #list(train_oof_df)
+
+    for m in models:
+        print(m)
+        #_stack_hist_plot(train_oof_df, test_oof_df, fold_indicator, [m])
+        #_dist_overview_plot(train_oof_df, test_oof_df, fold_indicator, [m])
+        _stack_hist_plot_facet(train_oof_df, y_train, test_oof_df, y_test, fold_indicator, [m])
+        plt.show()
+
+    return
+
+def _explore_val_vs_pred(X_train, y_train, X_test, y_test, label, oof_col_names, problem_type, eval_metric):
+
+    l1_fold_indicator = np.full((len(X_train),), np.nan)
+    for fold_i, (train_index, test_index) in enumerate(_get_ag_cv(layer=1).split(X_train, y_train)):
+        l1_fold_indicator[test_index] = fold_i
+
+    # _dist_overview_plot(X_train[oof_col_names], X_test[oof_col_names], l1_fold_indicator)
+    _plot_prediction_distributions(X_train[oof_col_names].copy(), y_train, X_test[oof_col_names].copy(), y_test, l1_fold_indicator)
+
+
+    with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", 1000):
+        print(X_train[oof_col_names].describe() - X_test[oof_col_names].describe())
+
+    return
+
 def _get_meta_data(l2_train_data, l2_test_data, label, oof_col_names, problem_type, eval_metric):
     # Init
     f_dup = oof_col_names + [label]
@@ -284,6 +666,12 @@ def _get_meta_data(l2_train_data, l2_test_data, label, oof_col_names, problem_ty
     X_test = l2_test_data.drop(columns=[label])
     y_test = l2_test_data[label]
 
+    _explore_val_vs_pred(X_train, y_train, X_test, y_test, label, oof_col_names, problem_type, eval_metric)
+
+    print('asd')
+    #print('train', _get_cheat_likelihood(l2_train_data, label, oof_col_names, eval_metric))
+    # print('test', _get_cheat_likelihood(l2_test_data, label, oof_col_names, eval_metric))
+    return {}
     # Compute metadata
     custom_meta_data = dict(
         train_l2_duplicates=sum(l2_train_data.duplicated()) / train_n_instances,
---
+++ scripts/leakage_benchmark/src/other/distribution_insights.py
@@ -0,0 +1,169 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+def get_fold_indicator(X_train, y_train, layer):
+    from autogluon.core.utils.utils import CVSplitter
+
+    cv = CVSplitter(n_splits=8, n_repeats=1, stratified=True, random_state=layer)
+
+    l1_fold_indicator = np.full((len(X_train),), np.nan)
+    for fold_i, (train_index, test_index) in enumerate(cv.split(X_train, y_train)):
+        l1_fold_indicator[test_index] = fold_i
+
+    return l1_fold_indicator
+
+
+def _viz_flip(l1_oof, l2_oof, y, title_postfix='', l1_model='L1/OOF/RandomForest_c1_BAG_L1',
+              l2_model='LightGBM_BAG_L2', l2_model_no_leak='LightGBM_mc_int_BAG_L2'):
+    plot_df = l1_oof.loc[:, [l1_model]]
+    plot_df['label'] = y
+    plot_df[l2_model] = l2_oof[l2_model]
+    plot_df[l2_model_no_leak] = l2_oof[l2_model_no_leak]
+    plot_df['fold'] = 'Train'
+    plot_df['error'] = abs(l1_oof[l1_model] - l2_oof[l2_model])
+
+    fig = go.Figure(data=go.Parcoords(
+        line=dict(color=plot_df['error'],
+                  showscale=True,
+                  colorscale='bluered'),
+        dimensions=[
+            dict(label=l1_model, range=[0, 1], values=plot_df[l1_model]),
+            dict(label='label', range=[0, 1], values=plot_df['label']),
+            dict(label=l2_model, range=[0, 1], values=plot_df[l2_model]),
+            dict(label=l2_model_no_leak, range=[0, 1], values=plot_df[l2_model_no_leak]),
+            dict(label='error', range=[0, 1], values=plot_df['error'])
+        ],
+
+    ), layout=dict(title=f'Label Flipping Insight{title_postfix}'))
+
+    fig.show()
+
+
+def _flip_model_check(X_train, y_train, X_test, y_test, l1_test_oof, l2_test_oof,
+                      l1_model, l2_model, predictor, eval_metric):
+    from sklearn.model_selection import cross_val_score
+    from lightgbm import LGBMClassifier
+
+    def bad_flips(label, l1_oof, l2_oof):
+        l1_loss = abs(label - l1_oof)
+        l2_loss = abs(label - l2_oof)
+        return (l2_loss - l1_loss) >= 0
+
+    # flip_X_train = pd.concat([X_train, l1_train_oof, l2_train_oof], axis=1)
+    # flip_y_train = bad_flips(y_train, l1_train_oof[l1_model].values, l2_train_oof[l2_model].values)
+
+    tmp_X_train = X_train.reset_index(drop=True).sample(n=min(len(X_train), 1000), random_state=42)
+    tmp_y_train = y_train.iloc[tmp_X_train.index]
+    tmp_y_train.reset_index(drop=True, inplace=True)
+    tmp_X_train.reset_index(drop=True, inplace=True)
+
+    # Add synthetic data
+    proba_scales = np.arange(0, 1.01, 0.01)
+    s_list = []
+    for p_s in proba_scales:
+        curr_scale = pd.Series(np.full(len(tmp_X_train), p_s), name=l1_model)
+
+        l2_X = pd.concat([tmp_X_train, curr_scale], axis=1)
+        l2_preds = pd.Series(
+            predictor.predict_proba(l2_X, model=l2_model, as_multiclass=False, as_pandas=False),
+            name=l2_model
+        )
+        mask = pd.Series(bad_flips(tmp_y_train, curr_scale.values, l2_preds.values), name='label')
+        s_list.append(pd.concat([l2_X, l2_preds, mask], axis=1))
+
+    synth_flip_data = pd.concat(s_list, axis=0)
+    synth_flip_X_train = synth_flip_data.drop(columns=['label'])
+    synth_flip_y_train = synth_flip_data['label'].values
+
+    flip_X_train = synth_flip_X_train  # pd.concat([flip_X_train, synth_flip_X_train])
+    flip_y_train = synth_flip_y_train  # np.hstack([flip_y_train, synth_flip_y_train])
+
+    sample_goal = np.unique(flip_y_train, return_counts=True)[1][1]
+    flip_X_train['class_label'] = flip_y_train
+    flip_X_train = flip_X_train.groupby('class_label').sample(n=sample_goal, random_state=42).reset_index(drop=True)
+    flip_y_train = flip_X_train['class_label'].values
+    flip_X_train = flip_X_train.drop(columns=['class_label'])
+
+    model_val_error = np.mean(cross_val_score(LGBMClassifier(random_state=42), flip_X_train, flip_y_train, cv=8,
+                                            scoring='balanced_accuracy'))
+
+    def flip_pred(_X, _y, _X_test, _to_flip_oof, _baseline_oof):
+        flip_chart = LGBMClassifier(random_state=42).fit(_X, _y).predict_proba(_X_test)[:, 1]
+        flip_mask = flip_chart >= 0.2
+        test_oof_tmp = _to_flip_oof.copy()
+        test_oof_tmp.loc[flip_mask] = _baseline_oof.loc[flip_mask]
+        return test_oof_tmp, flip_mask
+
+    flip_X_test = pd.concat([X_test, l1_test_oof, l2_test_oof], axis=1)
+    # Test Score
+    new_y_pred, flip_mask = flip_pred(flip_X_train, flip_y_train, flip_X_test, l2_test_oof[l2_model],
+                                      l1_test_oof[l1_model])
+    score = eval_metric(y_test, new_y_pred)
+    flip_ratio = sum(flip_mask) / len(flip_mask)
+    return score, flip_ratio, model_val_error
+
+
+def get_proba_insights(l1_train_oof, l1_test_oof, l2_train_oof, l2_test_oof, y_train, y_test,
+                       X_train, X_test, eval_metric, predictor):
+    # hue as unique yes or no
+
+    # 100 ros
+
+    # FLIP CHECKER
+    # incrementally increase from 0 to 1 over 1l probas and get diff to l2 probas
+    # with original features and for subset of instances
+    # get mean squared error or absolute error
+
+    # Algo
+    # need features and predictor for this
+
+    # l1_fold_indicator = get_fold_indicator(l1_train_oof, y_train, layer=1)
+
+    print('l1')
+    for bm in list(l1_train_oof):
+        val_score = eval_metric(y_train, l1_train_oof[bm])
+        test_score = eval_metric(y_test, l1_test_oof[bm])
+
+        print(bm, test_score, val_score)
+
+    print('l2')
+    for bm in list(l2_train_oof):
+        val_score = eval_metric(y_train, l2_train_oof[bm])
+        test_score = eval_metric(y_test, l2_test_oof[bm])
+
+        print(bm, test_score, val_score)
+
+    l1_model = list(l1_train_oof)[0]
+    l2_model = list(l2_train_oof)[0]
+
+    # # Get Val Score (technically leaks due to predictor I guess)
+    # from autogluon.core.utils.utils import CVSplitter
+    # cv = CVSplitter(n_splits=8, n_repeats=1, stratified=True, random_state=2)
+    # res, res_, res__ = [], [], []
+    # for train_index, test_index in cv.split(X_train, y_train):
+    #     score, flip_ratio, model_val_error = _flip_model_check(X_train.iloc[train_index], y_train[train_index],
+    #                                           X_train.iloc[test_index], y_train[test_index],
+    #                                           l1_train_oof.iloc[test_index], l2_train_oof.iloc[test_index],
+    #                                           l1_model, l2_model, predictor, eval_metric)
+    #     res.append(score)
+    #     res_.append(flip_ratio)
+    #     res__.append(model_val_error)
+    # print(f'CV Avoided Score: {np.mean(res)} (FR: {np.mean(res_)}) | Model Val Error: {np.mean(res__)}')
+
+
+    # Get Test Score
+    score, flip_ratio, model_val_error = _flip_model_check(X_train, y_train, X_test, y_test, l1_test_oof, l2_test_oof,
+                                          l1_model, l2_model, predictor, eval_metric)
+    print(f'Flip Avoided Test Score: {score} (FR: {flip_ratio}) | Model Val Score: {model_val_error}')
+
+    # --- 1 Base Model Viz ---
+    # l2_train_oof.columns = [x.replace('L1', 'L2') for x in l2_train_oof.columns]
+    # l2_test_oof.columns = [x.replace('L1', 'L2') for x in l2_test_oof.columns]
+    # _viz_flip(l1_train_oof, l2_train_oof, y_train, title_postfix=' for Train')
+    # _viz_flip(l1_test_oof, l2_test_oof, y_test, title_postfix=' for Test')
+    # input('Press enter to continue')
