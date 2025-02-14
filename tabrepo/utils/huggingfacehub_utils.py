6f041cb4eb8eff89c85033d28ce91eb9c40e7158
David  Salinas <david.salinas.pro@gmail.com>
2025-02-14 10:33:46 +0100

    Replace S3 backend with HF

--- tabrepo/utils/huggingfacehub_utils.py
+++ tabrepo/utils/huggingfacehub_utils.py
@@ -63,14 +63,16 @@ def upload_hugging_face(version: str, repo_id: str, override_existing_files: boo
 
 
 def download_from_huggingface(
-        version: str,
+        version: str = "2023_11_14",
         force_download: bool = False,
         local_files_only: bool = False,
         datasets: list[str] | None = None,
 ):
     """
+    :param version: name of a tabrepo version such as `2023_11_14`
     :param local_files_only: whether to use local files with no internet check on the Hub
     :param force_download: forces files to be downloaded
+    :param datasets: list of datasets to download, if not specified all datasets will be downloaded
     :return:
     """
     # https://huggingface.co/datasets/Tabrepo/tabrepo/tree/main/2023_11_14/model_predictions
@@ -81,7 +83,15 @@ def download_from_huggingface(
         allow_patterns = None
     else:
         allow_patterns = [f"*{version}*{d}*" for d in datasets]
-    print(allow_patterns)
+
+    allow_patterns += [
+        "*baselines.parquet",
+        "*configs.parquet",
+        "*baselines.parquet",
+        "*task_metadata.csv",
+    ]
+
+    print(f"Allowed patterns: {allow_patterns}")
     api.snapshot_download(
         repo_id="Tabrepo/tabrepo",
         repo_type="dataset",
@@ -98,7 +108,7 @@ if __name__ == '__main__':
     # )
     datasets = [
         'Australian',
-        'Bioresponse', 'GAMETES_Epistasis_2-Way_1000atts_0_4H_EDM-1_EDM-1_1', 'GAMETES_Epistasis_2-Way_20atts_0_1H_EDM-1_1', 'GAMETES_Epistasis_2-Way_20atts_0_4H_EDM-1_1', 'GAMETES_Epistasis_3-Way_20atts_0_2H_EDM-1_1', 'GAMETES_Heterogeneity_20atts_1600_Het_0_4_0_2_50_EDM-2_001', 'GAMETES_Heterogeneity_20atts_1600_Het_0_4_0_2_75_EDM-2_001', 'GesturePhaseSegmentationProcessed', 'Indian_pines', 'Internet-Advertisements', 'LED-display-domain-7digit', 'MiceProtein', 'OVA_Colon', 'OVA_Endometrium', 'OVA_Kidney', 'OVA_Lung', 'OVA_Ovary', 'OVA_Prostate', 'Satellite', 'SpeedDating', 'Titanic', 'ada', 'analcatdata_authorship', 'analcatdata_dmft', 'arcene', 'arsenic-female-bladder', 'autoUniv-au1-1000', 'autoUniv-au6-750', 'autoUniv-au7-1100', 'autoUniv-au7-700', 'balance-scale', 'bank32nh', 'bank8FM', 'baseball', 'blood-transfusion-service-center', 'boston_corrected', 'car', 'cardiotocography', 'churn', 'climate-model-simulation-crashes', 'cmc', 'cnae-9', 'colleges_usnews', 'cpu_act', 'cpu_small', 'credit-g', 'cylinder-bands', 'delta_ailerons', 'delta_elevators', 'diabetes', 'dna', 'dresses-sales', 'eucalyptus', 'fabert', 'first-order-theorem-proving', 'fri_c0_1000_5', 'fri_c0_500_5', 'fri_c1_1000_50', 'fri_c2_1000_25', 'fri_c2_500_50', 'fri_c3_1000_10', 'fri_c3_1000_25', 'fri_c3_500_10', 'fri_c3_500_50', 'fri_c4_500_100', 'gina', 'hill-valley', 'hiva_agnostic', 'ilpd', 'jasmine', 'kc1', 'kc2', 'kdd_el_nino-small', 'kin8nm', 'led24', 'madeline', 'madelon', 'mc1', 'meta', 'mfeat-factors', 'no2', 'optdigits', 'ozone-level-8hr', 'page-blocks', 'parity5_plus_5', 'pbcseq', 'pc1', 'pc2', 'pc3', 'pc4', 'philippine', 'phoneme', 'pm10', 'pollen', 'puma32H', 'puma8NH', 'qsar-biodeg', 'ringnorm', 'rmftsa_ladata', 'satimage', 'segment', 'semeion', 'spambase', 'splice', 'steel-plates-fault', 'sylvine', 'synthetic_control', 'tokyo1', 'twonorm', 'vehicle', 'visualizing_soil', 'volcanoes-a2', 'volcanoes-a3', 'volcanoes-a4', 'volcanoes-b5', 'volcanoes-d1', 'volcanoes-d4', 'volcanoes-e1', 'wall-robot-navigation', 'waveform-5000', 'wilt', 'wind', 'wine-quality-red', 'wine-quality-white']
+    ]
     download_from_huggingface(
         datasets=datasets,
         version="2023_11_14",
